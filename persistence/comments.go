53ea7a9ea76ce4df4fdff59247b5fa4e61bc5e4d
Anton Ptashnik <iavtomator@gmail.com>
2020-07-05 15:29:25 +0300

    implemented persistence-layer operations on comments, fixed comments table creation command

--- persistence/comments.go
+++ persistence/comments.go
@@ -1,21 +1,52 @@
 package persistence
 
 func AddComment(c Comment) (int64, error) {
-	return 0, errNotImpl
+	db := dbConn()
+	defer db.Close()
+	q := `INSERT INTO comments (text, task_id) VALUES ($1,$2) RETURNING id`
+	var id int64
+	err := db.QueryRow(q, c.Text, c.TaskID).Scan(&id)
+	return id, err
 }
 
 func GetComment(id int64) (Comment, error) {
-	return Comment{}, errNotImpl
+	db := dbConn()
+	defer db.Close()
+
+	var c Comment
+	row := db.QueryRow(`SELECT * FROM comments WHERE id=$1`, id)
+	err := row.Scan(&c.ID, &c.TaskID, &c.Text, &c.Date)
+	return c, err
 }
 
 func ListComments(taskID int64) ([]Comment, error) {
-	return nil, errNotImpl
+	db := dbConn()
+	defer db.Close()
+	q := `SELECT * FROM comments WHERE task_id=$1`
+	rows, _ := db.Query(q, taskID)
+	var comments []Comment
+	var c Comment
+	for rows.Next() {
+		err := rows.Scan(&c.ID, &c.TaskID, &c.Text, &c.Date)
+		if err != nil {
+			return comments, err
+		}
+		comments = append(comments, c)
+	}
+	return comments, nil
 }
 
 func DelComment(id int64) error {
-	return nil
+	db := dbConn()
+	defer db.Close()
+	res, err := db.Exec(`DELETE FROM comments WHERE id=$1`, id)
+	return verifyModified(res, err)
 }
 
 func UpdComment(c Comment) error {
-	return errNotImpl
+	db := dbConn()
+	defer db.Close()
+	q := `UPDATE comments SET text=$1,modified=DEFAULT WHERE id=$2`
+	res, err := db.Exec(q, c.Text, c.ID)
+	return verifyModified(res, err)
 }
--- persistence/comments_test.go
+++ persistence/comments_test.go
@@ -23,7 +23,7 @@ func TestComments(t *testing.T) {
 	t.Run("list", tests.list(data.task.ID))
 	t.Run("add", tests.add(data.task.ID))
 	t.Run("upd", tests.upd(data.comments[0]))
-	t.Run("del", tests.upd(data.comments[0]))
+	t.Run("del", tests.del(data.comments[0]))
 
 }
 
@@ -89,6 +89,7 @@ func (test *commentTests) get(expected Comment) func(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
+		actual.Date = expected.Date
 		if !reflect.DeepEqual(expected, actual) {
 			t.Error("expected/actual mismatch:", expected, actual)
 		}
@@ -138,6 +139,7 @@ func (test *commentTests) del(c Comment) func(t *testing.T) {
 func checkCommentExists(db *sql.DB, expected Comment) bool {
 	q := `SELECT * FROM comments WHERE id=$1`
 	var actual Comment
-	err := db.QueryRow(q, expected.ID).Scan(&actual.ID, &actual.TaskID, &actual.Text)
+	err := db.QueryRow(q, expected.ID).Scan(&actual.ID, &actual.TaskID, &actual.Text, &actual.Date)
+	actual.Date = expected.Date
 	return err == nil && reflect.DeepEqual(expected, actual)
 }
