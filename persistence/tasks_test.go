f69ac6f6db4ae3d7b41b17ac520005ae1a2b86c7
Anton Ptashnik <iavtomator@gmail.com>
2020-07-07 09:40:42 +0300

    fixed persistence layer tests for tasks

--- persistence/tasks_test.go
+++ persistence/tasks_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 type taskTests struct {
-	*sql.DB
+	TasksRepo
 }
 
 type taskTestsInput struct {
@@ -28,9 +28,9 @@ func TestTasks(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	tests := taskTests{db}
+	tests := taskTests{}
 	t.Run("get", tests.getTask(input.tasks))
-	t.Run("list", tests.listTasks(input.p))
+	t.Run("list", tests.listTasks(input.p.ID))
 	t.Run("add", tests.addTask(input.p, input.s, input.pri))
 	t.Run("upd", tests.updTask(input.tasks[0]))
 	t.Run("del", tests.delTask(input.tasks[0]))
@@ -44,12 +44,12 @@ func (test *taskTests) addTask(p Project, s Status, pri Priority) func(t *testin
 		Description: "test purpose entry",
 	}
 	return func(t *testing.T) {
-		taskID, err := AddTask(newTask)
+		taskID, err := test.TasksRepo.Add(newTask)
 		if err != nil {
 			t.Fatal(err)
 		}
 		newTask.ID = taskID
-		if !checkTaskExists(test.DB, newTask) {
+		if !checkTaskExists(newTask) {
 			t.Error("responded ok but entry not found")
 		}
 	}
@@ -57,7 +57,7 @@ func (test *taskTests) addTask(p Project, s Status, pri Priority) func(t *testin
 func (test *taskTests) getTask(expectedTasks []Task) func(*testing.T) {
 	return func(t *testing.T) {
 		for _, expectedTask := range expectedTasks {
-			actual, err := Get(expectedTask.ID)
+			actual, err := test.TasksRepo.Get(expectedTask.ID, expectedTask.ProjectID)
 			if err != nil {
 				t.Fatal(err)
 			}
@@ -67,14 +67,14 @@ func (test *taskTests) getTask(expectedTasks []Task) func(*testing.T) {
 		}
 	}
 }
-func (test *taskTests) listTasks(p Project) func(t *testing.T) {
+func (test *taskTests) listTasks(pid int64) func(t *testing.T) {
 	return func(t *testing.T) {
-		expected, err := List(p.ID)
+		expected, err := test.TasksRepo.List(pid)
 		if err != nil {
 			t.Fatal(err)
 		}
 		for _, task := range expected {
-			if !checkTaskExists(test.DB, task) {
+			if !checkTaskExists(task) {
 				t.Error("task not found:", task)
 			}
 		}
@@ -82,11 +82,11 @@ func (test *taskTests) listTasks(p Project) func(t *testing.T) {
 }
 func (test *taskTests) delTask(task Task) func(t *testing.T) {
 	return func(t *testing.T) {
-		err := Del(task.ID)
+		err := test.TasksRepo.Del(task.ID, task.ProjectID)
 		if err != nil {
 			t.Fatal(err)
 		}
-		if checkTaskExists(test.DB, task) {
+		if checkTaskExists(task) {
 			t.Error("responded ok but entry left in DB:", task)
 		}
 	}
@@ -96,11 +96,11 @@ func (test *taskTests) updTask(task Task) func(t *testing.T) {
 	return func(t *testing.T) {
 		updTask := task
 		updTask.Name += "_updated"
-		err := Upd(updTask)
+		err := test.TasksRepo.Upd(updTask)
 		if err != nil {
 			t.Fatal(err)
 		}
-		if !checkTaskExists(test.DB, updTask) {
+		if !checkTaskExists(updTask) {
 			t.Error("responded ok but updated entry not found. Old/new:", task, updTask)
 		}
 	}
@@ -143,7 +143,7 @@ func prepareTasks(db *sql.DB, p Project, s Status, pri Priority, n int) ([]Task,
 	return tasks, nil
 }
 
-func checkTaskExists(db *sql.DB, expected Task) bool {
+func checkTaskExists(expected Task) bool {
 	q := `SELECT * FROM tasks WHERE id=$1`
 	var actual Task
 	err := db.QueryRow(q, expected.ID).Scan(&actual.ID, &actual.StatusID, &actual.ProjectID, &actual.PriorityID, &actual.Name, &actual.Description)
