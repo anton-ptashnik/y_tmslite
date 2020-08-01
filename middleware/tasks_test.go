d1cbdf8faee791794a2fb21dde836bcac5559168
Anton Ptashnik <iavtomator@gmail.com>
2020-08-01 12:38:41 +0300

    improved middleware-layer tests on tasks

--- middleware/tasks_test.go
+++ middleware/tasks_test.go
@@ -4,10 +4,11 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
-	"fmt"
+	"github.com/go-chi/chi"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
+	"strconv"
 	"strings"
 	"testing"
 	"y_finalproject/persistence"
@@ -16,8 +17,13 @@ import (
 type tasksHandlerTest struct {
 	TasksHandler
 }
+
 type fakeTasksService struct {
-	tasks []persistence.Task
+	addOp func(task persistence.Task) (int64, error)
+	listOp func(pid int64) ([]persistence.Task,error)
+	delOp func(id int64, pid int64) error
+	updOp func(task persistence.Task) error
+	getOp func(id int64, pid int64) (persistence.Task,error)
 }
 
 func TestTasks(t *testing.T) {
@@ -30,39 +36,42 @@ func TestTasks(t *testing.T) {
 	t.Run("get", tests.getTask)
 }
 func (f *fakeTasksService) List(pid int64) ([]persistence.Task, error) {
-	return f.tasks, nil
+	return f.listOp(pid)
 }
 
 func (f *fakeTasksService) Del(id int64, pid int64) error {
-	return nil
+	return f.delOp(id, pid)
 }
 
 func (f *fakeTasksService) Add(task persistence.Task) (int64, error) {
-	id := len(f.tasks)
-	f.tasks = append(f.tasks, task)
-	return int64(id), nil
+	return f.addOp(task)
 }
 
 func (f *fakeTasksService) Upd(task persistence.Task) error {
-	if task.ID >= int64(len(f.tasks)) {
-		return errors.New("wrong ID")
-	}
-	f.tasks[task.ID] = task
-	return nil
+	return f.updOp(task)
 }
 
 func (f *fakeTasksService) Get(id int64, pid int64) (persistence.Task, error) {
-	if id >= int64(len(f.tasks)) {
-		return persistence.Task{}, errors.New("wrong ID")
-	}
-	return f.tasks[id], nil
+	return f.getOp(id, pid)
 }
 
 func (test *tasksHandlerTest) addTask(t *testing.T) {
-	body := `{	Name: 'abc', ProjectID: 0, StatusID: 0, 'PriorityID': 0 }`
+	body := `{	Name: 'abc', ProjectID: 1, StatusID: 0, 'PriorityID': 0 }`
+	var newTaskObj persistence.Task
+	json.NewDecoder(strings.NewReader(body)).Decode(&newTaskObj)
 
-	test.initTaskService([]persistence.Task{})
-	req := httptest.NewRequest("post", "/projects/1/tasks", strings.NewReader(body))
+	var tasks []persistence.Task
+	addTaskOp := func(task persistence.Task) (int64,error) {
+		tasks = append(tasks, task)
+		return task.ID,nil
+	}
+	test.TasksService = &fakeTasksService{
+		addOp: addTaskOp,
+	}
+
+	// /projects/1/tasks
+	ctx := context.WithValue(context.Background(), `pid`, 1)
+	req, _ := http.NewRequestWithContext(ctx, "post", "/", strings.NewReader(body))
 	w := httptest.NewRecorder()
 	test.TasksHandler.AddTask(w, req)
 
@@ -70,17 +79,32 @@ func (test *tasksHandlerTest) addTask(t *testing.T) {
 	var r struct{ ID int64 }
 	json.NewDecoder(resp.Body).Decode(&r)
 	if resp.StatusCode != http.StatusCreated {
-		t.Errorf("expected %v %v, but got %v", 201, resp.StatusCode, r.ID)
+		t.Errorf("expected code %v, but got %v", 201, resp.StatusCode)
+	}
+	if len(tasks) == 0 || !reflect.DeepEqual(tasks[0], newTaskObj) {
+		t.Errorf("repo expected to contain %v, but not found. Repo: %v", newTaskObj, tasks)
 	}
 }
 
 func (test *tasksHandlerTest) listTasks(t *testing.T) {
-	expectedTasks := []persistence.Task{{ProjectID: 2, StatusID: 3, PriorityID: 1, Name: "test"}}
+	var pid int64 = 2
+	expectedTasks := []persistence.Task{{ProjectID: pid, StatusID: 3, PriorityID: 1, Name: "test"}}
 
-	expectedTasks = test.initTaskService(expectedTasks)
-	req := httptest.NewRequest("get", "/projects/1/tasks", nil)
+	listTasksOp := func(_pid int64) ([]persistence.Task,error) {
+		if _pid != pid {
+			return nil, errors.New("not found")
+		}
+		return expectedTasks, nil
+	}
+
+	test.TasksService = &fakeTasksService{
+		listOp: listTasksOp,
+	}
+	ctx := chi.NewRouteContext()
+	ctx.URLParams.Add("pid", strconv.FormatInt(pid, 10))
+	req := httptest.NewRequest("get", "/", nil)
 	w := httptest.NewRecorder()
-	test.TasksHandler.ListTasks(w, req)
+	test.TasksHandler.ListTasks(w, setCtx(req, ctx))
 
 	resp := w.Result()
 	var actualTasks []persistence.Task
@@ -92,20 +116,27 @@ func (test *tasksHandlerTest) listTasks(t *testing.T) {
 
 func (test *tasksHandlerTest) getTask(t *testing.T) {
 	expectedTask := persistence.Task{
+		ID: 4,
 		ProjectID:  1,
-		StatusID:   1,
-		PriorityID: 0,
 		Name:       "test",
 	}
-	expectedTask = test.initTaskService([]persistence.Task{
-		expectedTask,
-	})[0]
+	getTaskOp := func(_tid int64, _pid int64) (persistence.Task,error) {
+		if _tid != expectedTask.ID || _pid != expectedTask.ProjectID {
+			return persistence.Task{}, errors.New("not found")
+		}
+		return expectedTask, nil
+	}
+	test.TasksService = &fakeTasksService{
+		getOp: getTaskOp,
+	}
 	handler := test.TasksHandler.GetTask
 
-	endpoint := "/projects/1/tasks/2"
-	req := httptest.NewRequest("get", endpoint, nil)
+	ctx := chi.NewRouteContext()
+	ctx.URLParams.Add("tid", strconv.FormatInt(expectedTask.ID, 10))
+	ctx.URLParams.Add("pid", strconv.FormatInt(expectedTask.ProjectID, 10))
+	req := httptest.NewRequest("get", "/", nil)
 	w := httptest.NewRecorder()
-	handler(w, req)
+	handler(w, setCtx(req, ctx))
 
 	resp := w.Result()
 	var actual persistence.Task
@@ -116,59 +147,81 @@ func (test *tasksHandlerTest) getTask(t *testing.T) {
 }
 
 func (test *tasksHandlerTest) delTask(t *testing.T) {
-	task := test.initTaskService([]persistence.Task{
-		{
-			Name: "testtask",
-		},
-	})[0]
+	var tid int64 = 4
+	var pid int64 = 5
+	deleted := false
+	delTaskOp := func(_tid int64, _pid int64) error {
+		if _tid != tid || _pid != pid {
+			return errors.New("not found")
+		}
+		deleted = true
+		return nil
+	}
+	test.TasksService = &fakeTasksService{
+		delOp: delTaskOp,
+	}
 
 	handler := test.TasksHandler.DelTask
-	endpoint := fmt.Sprint("/projects/0/tasks/",task.ID)
-	req := httptest.NewRequest("delete", endpoint, nil)
+
+	req := httptest.NewRequest("delete", "/", nil)
 	w := httptest.NewRecorder()
-	handler(w, req)
+
+	ctx := chi.NewRouteContext()
+	ctx.URLParams.Add("pid", strconv.FormatInt(pid, 10))
+	ctx.URLParams.Add("tid", strconv.FormatInt(tid, 10))
+	handler(w, setCtx(req, ctx))
 
 	resp := w.Result()
 	if resp.StatusCode != http.StatusOK {
 		t.Error("expected/actual status mismatch:", 200, resp.StatusCode)
 	}
+	if !deleted {
+		t.Error("indicated ok but entry still exists")
+	}
 }
 
 func (test *tasksHandlerTest) updTask(t *testing.T) {
 	task := persistence.Task{
-		ProjectID:  1,
 		StatusID:   1,
-		PriorityID: 0,
+		PriorityID: 7,
 		Name:       "test",
 	}
-	task = test.initTaskService([]persistence.Task{
-		task,
-	})[0]
+	updTaskOp := func(_task persistence.Task) error {
+		if _task.ID != task.ID || _task.ProjectID != task.ProjectID {
+			return errors.New("not found")
+		}
+		task = _task
+		return nil
+	}
+	getTaskOp := func(tid int64, pid int64) (persistence.Task,error) {
+		return task, nil
+	}
+	test.TasksService = &fakeTasksService{
+		updOp: updTaskOp,
+		getOp: getTaskOp,
+	}
 	body := `{ "Name": "newName" }`
 	expectedTask := persistence.Task{
 		Name: "newName",
 	}
 	handler := test.TasksHandler.UpdTask
 
-	//endpoint := "/projects/1/tasks/2"
-	ctx := context.WithValue(context.Background(), `pid`, task.ProjectID)
-	ctx = context.WithValue(ctx, `tid`, task.ID)
-	req, _ := http.NewRequestWithContext(ctx, "put", `/`, strings.NewReader(body))
+	ctx := chi.NewRouteContext()
+	ctx.URLParams.Add(`pid`, strconv.FormatInt(task.ProjectID, 10))
+	ctx.URLParams.Add(`tid`, strconv.FormatInt(task.ID, 10))
+	req := httptest.NewRequest("put", `/`, strings.NewReader(body))
 	w := httptest.NewRecorder()
-	handler(w, req)
+	handler(w, setCtx(req, ctx))
 
 	resp := w.Result()
-	updatedTask, _ := test.TasksService.Get(expectedTask.ID, expectedTask.ProjectID)
-	if resp.StatusCode != http.StatusOK || !reflect.DeepEqual(expectedTask, updatedTask) {
+	if resp.StatusCode != http.StatusOK {
 		t.Error("expected/actual status mismatch:", 200, resp.StatusCode)
+	}
+	if !reflect.DeepEqual(expectedTask, task) {
+		t.Error("indicated ok but seem was not updated, expected/actual:", expectedTask, task)
 	}
 }
 
-func (test *tasksHandlerTest) initTaskService(tasks []persistence.Task) []persistence.Task {
-	taskService := fakeTasksService{}
-	for i := range tasks {
-		tasks[i].ID, _ = taskService.Add(tasks[i])
-	}
-	test.TasksService = &taskService
-	return tasks
+func setCtx(r *http.Request, ctx *chi.Context) *http.Request {
+	return r.WithContext(context.WithValue(r.Context(), chi.RouteCtxKey, ctx))
 }
